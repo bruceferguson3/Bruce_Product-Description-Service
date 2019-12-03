b07e3368844dc877700f1ef327b19e75392212aa
BruceFergusonIII <bruce.ferguson3@gmail.com>
2019-12-03 15:06:12 -0600

    Add styling to the 'new' and 'onSale Price' components

--- client/Components/TopPackage.jsx
+++ client/Components/TopPackage.jsx
@@ -10,7 +10,7 @@ const TopPackage = (props) => {
             {props.pNew === 1 ? (<p className='newBox'>New</p>) : (<span hidden/>)}
             <h1 className='productHeading'>{props.pName}</h1>
             <div className='productDescr'>{props.pDescr}</div>
-            <div className='productHeading'>{`$${props.pPrice}`}</div>
+            <div className='productHeading'>{props.pOnSale === 1 ? <span className='salePrice'>{`$${props.pPrice}`}</span> : `$${props.pPrice}`}</div>
             <div className='productDescr'>
               <span className='starContainer'>
                   <span className='starColor'/>
@@ -20,8 +20,7 @@ const TopPackage = (props) => {
               <span className='productDescr'>{props.pReviewCounter !== 1 ? `${props.pReviewCounter} Reviews` :  `${props.pReviewCounter} Review`}</span>
             </div>
             <div className='benefitDescr'>
-                {props.pBenefit}
-                <a className='readMore'>Read More</a>
+                {props.pBenefit !== null ? <span>{props.pBenefit} <a className='readMore'> Read More </a></span> : ''}
             </div>
         </div>
 
